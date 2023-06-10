808b55d7caeb78c261b38d1da175819f39df8147
Arseniy Khvorov <khvorov45@gmail.com>
2023-06-10 18:54:26 +1000

    move generated code into a file

--- build.bat
+++ build.bat
@@ -2,5 +2,4 @@
 set "scriptdir=%~dp0"
 set "filename=%scriptdir%\build"
 set "exename=%filename%.exe"
-clang -g -Wall -Wextra %filename%.c -o %exename% -Wl,-incremental:no
-%exename% %*
+clang -g -Wall -Wextra %filename%.c -o %exename% -Wl,-incremental:no && %exename% %*
