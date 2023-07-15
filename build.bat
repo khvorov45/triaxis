11d1fccf17ee7c87162887cb47c9260336e53ee6
Arseniy Khvorov <khvorov45@gmail.com>
2023-07-15 21:19:24 +1000

    simplify build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,2 @@
 @echo off
-set "scriptdir=%~dp0"
-set "filename=%scriptdir%\build"
-set "exename=%filename%.exe"
-clang -g -Wall -Wextra %filename%.c -o %exename% -Wl,-incremental:no && %exename% %*
+clang -g -Wall -Wextra build.c -o build.exe && build.exe %*
